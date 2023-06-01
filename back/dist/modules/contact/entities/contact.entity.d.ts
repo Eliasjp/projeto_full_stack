bc09fe0ffc03f84a858729d8eb0d55a8691c93e2
Elias Jin Park <110125906+Eliasjp@users.noreply.github.com>
2023-06-01 14:05:46 -0300

    Delete back/dist directory
    git ignored folder

--- back/dist/modules/contact/entities/contact.entity.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Contact {
-    readonly id: string;
-    full_name: string;
-    email: string;
-    phone: string;
-    created_at: Date;
-    client_id?: string;
-}
--- back/dist/modules/contact/repository/contact.repository.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ContactRepository = void 0;
-class ContactRepository {
-}
-exports.ContactRepository = ContactRepository;
-//# sourceMappingURL=contact.repository.js.map
