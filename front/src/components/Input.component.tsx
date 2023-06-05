9a2fa23eef3fd96908036b55c8d47a80382a9915
eliasjp <illumine1998@gmail.com>
2023-06-05 02:03:05 -0300

    stop prod for cors implementation

--- front/src/components/Input.component.tsx
+++ front/src/components/Input.component.tsx
@@ -2,14 +2,16 @@
 interface InputSchema {
     inputPlaceholder: string
     inputType?: string
+    toForm: any
 }
 
-export function Input ({inputPlaceholder, inputType="text"}: InputSchema){
+export function Input ({inputPlaceholder, toForm, inputType="text"}: InputSchema){
 
     return (
         <input 
         placeholder={inputPlaceholder} 
         type={inputType}
+        {...toForm}
         className="h-12 py-2 px-1 border-gray-500 border rounded-md"
         />
     )
